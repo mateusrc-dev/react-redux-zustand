6f30f4ab818ec6b9d659b867092a9eb656281637
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-08-03 10:33:50 -0300

    created component Player

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
+import "./styles/global.css";
+
 import { Provider as ReduxProvider } from "react-redux";
 
-import { AddTodo } from "./components/AddTodo";
-import { TodoList } from "./components/TodoList";
 import { store } from "./store";
+import { Player } from "./pages/player";
 
 export function App() {
   return (
     // ReduxProvider will send all information's of context for the components
     <ReduxProvider store={store}>
-      <TodoList />
-      <AddTodo />
+      <Player />
     </ReduxProvider>
   );
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,27 +1,11 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import { useSelector, TypedUseSelectorHook } from "react-redux";
-
-// here will have just a part of state, we can do this, created parts the global state (store)
-const todoSlice = createSlice({
-  name: "todo",
-  initialState: ["Fazer café", "Estudar redux", "Estudar Zustand"],
-  reducers: {
-    // here we are going to say what actions the interface can do in our state
-    add: (state, action) => {
-      state.push(action.payload.newTodo);
-    },
-  },
-});
 
 // redux use the context api
 // store is the global state
 export const store = configureStore({
-  reducer: {
-    todo: todoSlice.reducer,
-  },
+  reducer: {},
 });
-
-export const { add } = todoSlice.actions;
 
 // function getState returns all state, and ReturnType get the format of state
 export type RootState = ReturnType<typeof store.getState>;
