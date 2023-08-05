868aa0dafbaecbdfde5d6adc083006dbddcd0637
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-08-04 21:15:30 -0300

    updated project

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { useSelector, TypedUseSelectorHook } from "react-redux";
+import { player } from "../pages/slices/player";
 
 // redux use the context api
 // store is the global state
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    player: player,
+  },
 });
 
 // function getState returns all state, and ReturnType get the format of state
